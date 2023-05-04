c0e7219a653d70e95c01ddb70e1e1cec2dbe4b87
Isuru M Soysa <isurumsoysa@gmail.com>
2023-05-05 00:16:25 +0530

    Update firebase-config.js

--- Frontend/src/firebase-config.js
+++ Frontend/src/firebase-config.js
@@ -5,13 +5,14 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBatOQMRd6Btp8ttsyBd_QZTwbeFxffvK4",
-  authDomain: "expenses-tracker-8cee4.firebaseapp.com",
-  projectId: "expenses-tracker-8cee4",
-  storageBucket: "expenses-tracker-8cee4.appspot.com",
-  messagingSenderId: "43273886495",
-  appId: "1:43273886495:web:7a425c2fa412e98a3c6e2a",
-  measurementId: "G-8Z84TPQN18",
+  //apikey: process.env.REACT_APP_ET_API_KEY,
+  apikey: import.meta.env.VITE_ET_API_KEY,
+  authDomain: import.meta.env.ET_AUTH_DOMAIN,
+  projectId: import.meta.env.ET_PROJECT_IT,
+  storageBucket: import.meta.env.ET_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.ET_MESSAGE_SENDER_ID,
+  appId: import.meta.env.ET_APP_ID,
+  measurementId: import.meta.env.ET_MESUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
